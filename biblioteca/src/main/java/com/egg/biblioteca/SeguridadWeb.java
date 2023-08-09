0a1082f8a6bef1179b3df8a8f67628a4465df240
marcos-git01 <marcoslefama02@gmail.com>
2023-08-09 09:01:42 -0300

    Commit 4: Modificaciones V3 y V4

--- biblioteca/src/main/java/com/egg/biblioteca/SeguridadWeb.java
+++ biblioteca/src/main/java/com/egg/biblioteca/SeguridadWeb.java
@@ -1,16 +1,29 @@
 
 package com.egg.biblioteca;
 
+import com.egg.biblioteca.servicios.UsuarioServicio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SeguridadWeb extends WebSecurityConfigurerAdapter {
+    
+    @Autowired
+    public UsuarioServicio usuarioServicio;
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(usuarioServicio)
+                .passwordEncoder(new BCryptPasswordEncoder());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
