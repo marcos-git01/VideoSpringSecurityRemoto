268d4b8be18f3b1173e4c0acc141211a54569f61
marcos-git01 <marcoslefama02@gmail.com>
2023-08-10 22:19:13 -0300

    Commit 9: Modificaciones V8

--- biblioteca/src/main/java/com/egg/biblioteca/SeguridadWeb.java
+++ biblioteca/src/main/java/com/egg/biblioteca/SeguridadWeb.java
@@ -42,7 +42,7 @@ public class SeguridadWeb extends WebSecurityConfigurerAdapter {
                         .permitAll()
                 .and().logout()
                         .logoutUrl("/logout")
-                        .logoutSuccessUrl("/") //logoutSuccessUrl("/login")
+                        .logoutSuccessUrl("/login") //logoutSuccessUrl("/login")
                         .permitAll()
                 .and().csrf()
                         .disable();
